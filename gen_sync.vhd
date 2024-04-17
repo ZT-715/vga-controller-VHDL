637f118ee3a172617117eb8c2d8a197064ab9ebc
Gabriel S.Zanella <gabes.zanella@gmail.com>
2024-04-17 11:03:46 -0300

    Update gen_sync: Make parameters more generic
    Changed the generic time parameters to the minumum required to work.
    Now the component is more flexible and may be used to signal when the
    display is in addressable range.

--- gen_sync.vhd
+++ gen_sync.vhd
@@ -4,34 +4,36 @@ use ieee.numeric_std.all;
 
 
 entity gen_sync is
-
     generic(
     -- pixel count of each interval
-        ADDRESSABLE, FRONT_PORCH, SYNC, BACK_PORCH: integer := (800, 56, 120, 64);
-        COUNTER_BITS: integer := 11
+        LOW, PULSE integer := (800 + 56, 120);
+        COUNTER_LENGTH: integer := 11
     );
 
     port(
-        c: in std_logic_vector(COUNTER_BITS-1 downto 0);
+        c: in std_logic_vector(COUNTER_LENGTH-1 downto 0);
         sync: out std_logic
     );
 
 begin
 
-    assert ADDRESSABLE + FRONT_PORCH + SYNC + BACK_PORCH <= 2**COUNTER_BITS
+    assert LOW + PULSE <= 2**COUNTER_LENGTH
     report "Counter range doesnt reach total time range."
     severity failure;
+
 end entity gen_sync;
 
 architecture imp of gen_sync is
-    signal count: unsigned(COUNTER_BITS-1 downto 0);
+    signal count: unsigned(COUNTER_LENGTH-1 downto 0);
+    signal 
 begin
     count <= unsigned(c);
 
     with count select
         sync =>
-            '0' when,
-            '1' when,
+            '1' when 0,
+            '0' when LOW - 1,
+            '1' when LOW+PULSE - 1,
             unaffected when others;
 
 end architecture;
