e979929e052eb02ddb0b3b260055cd90b03a501c
Gabriel S.Zanella <gabes.zanella@gmail.com>
2024-05-04 13:54:28 -0300

    Fix generic counter and add testbench

--- gen_counter.vhd
+++ gen_counter.vhd
@@ -3,29 +3,25 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 entity gen_counter is
-
     generic(
-        LIMIT: integer := 1040;
-        COUNTER_LENGTH: integer := 11
+        LIMIT           : integer := 1040;
+        COUNTER_LENGTH  : integer := 11
     );
-
     port(
-        rst, clk, en: in std_logic;
-        y: out std_logic_vector(COUNTER_LENGTH-1 downto 0));
-
+        rst, clk, en   : in  std_logic;
+        y              : out std_logic_vector(COUNTER_LENGTH-1 downto 0)
+    );
 end entity;
 
 architecture imp of gen_counter is
-    signal count: unsigned(COUNTER_LENGTH-1 downto 0);
 begin
-    
-    process(clk, rst, en)
-        variable count: natural range 0 to LIMIT;
+    process(clk)
+	 variable count: natural range 0 to LIMIT := 0;
     begin
-        if rst = '1' then 
-            count := 0;
-        elsif rising_edge(clk) then
-            if en = '1' then
+        if rising_edge(clk) then
+            if rst = '1' then
+                count := 0; 
+            elsif en = '1' then
                 if count = LIMIT then
                     count := 0;
                 else
@@ -33,8 +29,8 @@ begin
                 end if;
             end if;
         end if;
-        y <= std_logic_vector(to_unsigned(count, COUNTER_LENGTH));
+		    y <= std_logic_vector(to_unsigned(count, COUNTER_LENGTH));
     end process;
 
-
 end architecture;
+
---
+++ gen_counter_tb.vhd
@@ -0,0 +1,54 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity gen_counter_tb is 
+
+end entity;
+
+architecture tb of gen_counter_tb is
+constant LIMIT: natural := 100;
+constant COUNTER_LENGHT: natural := 8;
+
+signal rst, clk, en: std_logic := '1';
+signal y: std_logic_vector(COUNTER_LENGHT-1 downto 0);
+
+begin
+
+	clk <= not clk after 10 ns;
+	rst <= '1', '0' after 20 ns;
+	en <= '0', '1' after 50 ns;
+	
+	uut: entity work.gen_counter(imp) 
+					generic map(LIMIT, COUNTER_LENGHT)
+					port map(rst, clk, en, y);
+					
+	process
+		variable counting: natural range 0 to 2*LIMIT := 0;
+		variable counted: natural range 0 to 2*LIMIT;
+		begin
+			wait until en = '1';
+		
+			while (counting <= LIMIT) loop
+		
+					counted := to_integer(unsigned(y));
+										
+					report time'image(now) & " - " & integer'image(counting) & 
+					" vs. " & integer'image(counted);
+			
+					if(counting < LIMIT) then
+						assert counting = counted
+						severity failure;
+					else 
+						assert counted < LIMIT
+						severity failure;
+					end if;
+					
+				   wait for 20 ns;
+					counting := counting + 1;
+			end loop;
+			wait;
+	end process;
+end architecture;
+													
+	
