9074d9d084980858e3419cf2093a1ce79404728d
Gabriel S.Zanella <gabes.zanella@gmail.com>
2024-05-29 11:55:20 -0300

    Fix sync: start count must be greater than 0

--- gen_sync.vhd
+++ gen_sync.vhd
@@ -6,7 +6,10 @@ use ieee.numeric_std.all;
 entity gen_sync is
     generic(
     -- pixel count of each interval
+		  HIGH_START_COUNT: natural := 56; 
         LOW_COUNT: natural := 120;
+		  TOTAL_COUNT: natural := 1040;
+		  
         COUNTER_LENGTH: natural := 11
     );
 
@@ -18,67 +21,76 @@ entity gen_sync is
 	 
 end entity gen_sync;
 
-architecture tb of gen_sync is
-	-- signal count: integer range 0 to 2**COUNTER_LENGTH - 1;
-begin
-	
---    count <= to_integer(unsigned(c));
-
-	process
-	begin
-	sync <= '0';
---	report integer'image(count) & "Start";
-	wait for LOW_COUNT * 20 ns;
-	sync <= '1';
---	report integer'image(count) & "End sync";
-	end process;
-   			 
-end architecture;
-
-architecture imp of gen_sync is
-	signal count: integer range 0 to 2**COUNTER_LENGTH - 1;
-	signal d: std_logic;
-begin
-	
-    count <= to_integer(unsigned(c));
-
-	process(count)
-	begin
---		report integer'image(count);
-		if(count >= LOW_COUNT-1) then
-			d <= '1';
-		else 
-		  d <= '0';
-		end if;
-	end process;
-   			 
-   sync <= d;
-
-end architecture;
-
---architecture imp2 of gen_sync is
---	signal count: unsigned(COUNTER_LENGTH - 1 downto 0);
---	signal d, q: std_logic;
+--architecture imp0 of gen_sync is
+--	signal count: integer range 0 to 2**COUNTER_LENGTH - 1;
+--	signal d: std_logic;
 --begin
 --	
---	 dff: entity work.dflipflop(imp) port map(rst, clk, d, q);
+--    count <= to_integer(unsigned(c));
 --
---    count <= (unsigned(c));
---
---	process(count, q)
+--	process(count)
 --	begin
---		-- report integer'image(count);
---		if(count = 0) then
---			d <= '0';
---		elsif(count = LOW-2) then
+----		report integer'image(count);
+--		if(count >= LOW_COUNT-1) then
 --			d <= '1';
---		else
---			d <= q;
+--		else 
+--		  d <= '0';
 --		end if;
 --	end process;
 --   			 
---   sync <= q;
+--   sync <= d;
 --
 --end architecture;
+
+--architecture tb of gen_sync is
+--	-- signal count: integer range 0 to 2**COUNTER_LENGTH - 1;
+--begin
+--	
+----    count <= to_integer(unsigned(c));
+--
+--	process
+--	begin
+--	sync <= '0';
+----	report integer'image(count) & "Start";
+--	wait for LOW_COUNT * 20 ns;
+--	sync <= '1';
+----	report integer'image(count) & "End sync";
+--	end process;
+--   			 
+--end architecture;
+
+architecture imp of gen_sync is
+	signal count: integer range 0 to 2**COUNTER_LENGTH - 1;
+	signal d, q: std_logic;
+begin
+	
+	assert HIGH_START_COUNT > 0 	
+	report "HIGH_START_COUNT must be 1 or greater"
+	severity failure;
+	
+	 dff: entity work.dflipflop(imp) 
+			port map(clk => clk,
+						rst => rst,
+						d => d,
+						q => q);
+
+    count <= to_integer(unsigned(c));
+	 
+	process(count, q)
+	begin
+		if(count = TOTAL_COUNT - 1) then
+			d <= '1';
+		elsif(count = HIGH_START_COUNT - 1) then
+			d <= '0';
+		elsif(count = HIGH_START_COUNT + LOW_COUNT - 1) then
+			d <= '1';
+		else
+			d <= q;
+		end if;
+	end process;
+   			 
+   sync <= q;
+
+end architecture;
 --    
 
--- vga_controller.vhd
+++ vga_controller.vhd
@@ -6,14 +6,14 @@ entity vga_controller is
 
     generic(
         H_ADDRESSABLE: natural := 800;
-        H_FRON_PORCH: natural := 56; 
+        H_FRONT_PORCH: natural := 56; 
         H_SYNC: natural := 120; 
-        H_BACH_PORCH: natural := 64;
+        H_BACK_PORCH: natural := 64;
 
         V_ADDRESSABLE: natural := 600; 
-        V_FRON_PORCH: natural := 37; 
+        V_FRONT_PORCH: natural := 37; 
         V_SYNC: natural := 6; 
-        V_BACV_PORCH: natural := 23;
+        V_BACK_PORCH: natural := 23;
 
         H_COUNTER_LENGTH: natural := 11;
         V_COUNTER_LENGTH: natural := 10;
@@ -21,6 +21,7 @@ entity vga_controller is
         ADDR_LINE_LENGTH: natural := 10;
         ADDR_COLUMN_LENGTH: natural := 10
     );
+	 
     port(
         rst, clk: in std_logic;
         hsync, vsync: out std_logic;
@@ -48,23 +49,23 @@ begin
     b <= (others => '1');
 
     -- Enable contagem vertical por 1 pixel a cada ciclo da contagem horizontal
-    v_enable <= '1' when h_counter(H_COUNTER_LENGTH - 1 downto 1) = H_COUNTER_START(H_COUNTER_LENGTH - 1 downto 1) else '0';
+    v_enable <= '1' when h_counter = H_COUNTER_START else '0';
 
---     h_address <= std_logic_vector(h_counter(ADDR_LINE_LENGTH-1 downto 0));
---     v_address <= std_logic_vector(v_counter(ADDR_COLUMN_LENGTH-1 downto 0));
 
     -- Contagem de pixels para lógica de sincronismo horizontal
     h_count: entity work.gen_counter(imp)
-            generic map(LIMIT => H_ADDRESSABLE + H_FRON_PORCH + H_SYNC + H_BACH_PORCH,
+            generic map(LIMIT => H_FRONT_PORCH + H_SYNC + H_BACK_PORCH + H_ADDRESSABLE,
                         COUNTER_LENGTH => H_COUNTER_LENGTH)
             port map(rst => rst,
                      clk => clk,
                      en => '1',
-                     y => h_counter);
-
+                     y => h_counter);	
+							
     -- Gera indicador de sincronismo horizontal
     horizontal_sync: entity work.gen_sync(imp)
-            generic map(LOW_COUNT =>  H_SYNC,
+            generic map(HIGH_START_COUNT => H_FRONT_PORCH,
+								LOW_COUNT =>  H_SYNC,
+								TOTAL_COUNT => H_FRONT_PORCH + H_SYNC + H_BACK_PORCH + H_ADDRESSABLE,
                         COUNTER_LENGTH => H_COUNTER_LENGTH)
             port map(clk => clk,
 							rst => rst,
@@ -73,7 +74,9 @@ begin
 
     -- Gera indicador de zone horizontal endereçável
     horizontal_address: entity work.gen_sync(imp)
-            generic map(LOW_COUNT => H_FRON_PORCH + H_SYNC + H_BACH_PORCH,
+            generic map(HIGH_START_COUNT => 1,
+								LOW_COUNT => H_FRONT_PORCH + H_SYNC + H_BACK_PORCH,
+								TOTAL_COUNT => H_FRONT_PORCH + H_SYNC + H_BACK_PORCH + H_ADDRESSABLE,
                         COUNTER_LENGTH => H_COUNTER_LENGTH)
             port map(clk => clk,
 							rst => rst,
@@ -91,7 +94,7 @@ begin
 
     -- Contagem de linhas para geradores de lógica de sincronismo vertical
     v_count: entity work.gen_counter(imp)
-            generic map(LIMIT => V_ADDRESSABLE + V_FRON_PORCH + V_SYNC + V_BACV_PORCH,
+            generic map(LIMIT => V_FRONT_PORCH + V_SYNC + V_BACK_PORCH + V_ADDRESSABLE,
                         COUNTER_LENGTH => V_COUNTER_LENGTH)
             port map(rst => rst,
                      clk => clk,
@@ -100,7 +103,9 @@ begin
 
     -- Gera indicador de sincronia vertical
     vertical_sync: entity work.gen_sync(imp)
-            generic map(LOW_COUNT =>  V_SYNC, 
+            generic map(HIGH_START_COUNT => V_FRONT_PORCH,
+								LOW_COUNT =>  V_SYNC,
+								TOTAL_COUNT => V_FRONT_PORCH + V_SYNC + V_BACK_PORCH + V_ADDRESSABLE,
                         COUNTER_LENGTH => V_COUNTER_LENGTH)
             port map(clk => clk,
 							rst => rst,
@@ -109,7 +114,9 @@ begin
  
     -- Gera indicador de zona vertical endereçável
     vertical_address: entity work.gen_sync(imp)
-            generic map(LOW_COUNT => V_FRON_PORCH + V_SYNC + V_BACV_PORCH, 
+            generic map(HIGH_START_COUNT => 1,
+								LOW_COUNT => V_FRONT_PORCH + V_SYNC + V_BACK_PORCH, 
+								TOTAL_COUNT => V_FRONT_PORCH + V_SYNC + V_BACK_PORCH + V_ADDRESSABLE,
                         COUNTER_LENGTH => V_COUNTER_LENGTH)
             port map(clk => clk,
 							rst => rst,
