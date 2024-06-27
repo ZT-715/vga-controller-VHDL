726c9fcf8bbe12a1e00a0be5735f0bb760c4a5da
Gabriel S.Zanella <gabes.zanella@gmail.com>
2024-06-27 03:34:49 -0300

    Fix vertical assert timming
    TODO:
    
    Remake tb with time asserts after wait until statements, and
    returns on console measures of signal form intead of only OKs.

--- vga_controller_tb.vhd
+++ vga_controller_tb.vhd
@@ -1,3 +1,6 @@
+-- Reescrever com loops e timedeltas para validar mudanças ao invés de estados
+-- afinal, o modelsim não lê estados corretamente...
+
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
@@ -9,16 +12,22 @@ entity vga_controller_tb is
   
   constant CLOCK_PERIOD: time := 20 ns;
   
-  constant H_ADDRESSABLE: natural := 800;
-  constant H_FRONT_PORCH: natural := 56; 
-  constant H_SYNC: natural := 120; 
-  constant H_BACK_PORCH: natural := 64;
+  constant H_ADDRESSABLE: natural := 8;
+  constant H_FRONT_PORCH: natural := 5; 
+  constant H_SYNC: natural := 4; 
+  constant H_BACK_PORCH: natural := 4;
   constant H_PERIOD: natural := H_BACK_PORCH + H_SYNC + H_FRONT_PORCH + H_ADDRESSABLE;
+ 
+--    constant H_ADDRESSABLE: natural := 800;
+--    constant H_FRONT_PORCH: natural := 56; 
+--    constant H_SYNC: natural := 120; 
+--    constant H_BACK_PORCH: natural := 64;
+--    constant H_PERIOD: natural := H_BACK_PORCH + H_SYNC + H_FRONT_PORCH + H_ADDRESSABLE;
 
-  constant V_ADDRESSABLE: natural := 600; 
-  constant V_FRONT_PORCH: natural := 37; 
-  constant V_SYNC: natural := 6; 
-  constant V_BACK_PORCH: natural := 23;
+  constant V_ADDRESSABLE: natural := 6; 
+  constant V_FRONT_PORCH: natural := 3; 
+  constant V_SYNC: natural := 3; 
+  constant V_BACK_PORCH: natural := 3;
   constant V_PERIOD: natural := V_BACK_PORCH + V_SYNC + V_FRONT_PORCH + V_ADDRESSABLE;
   
   constant BLACK: std_logic_vector(11 downto 0) := (others => '0');
@@ -38,16 +47,28 @@ architecture tb of vga_controller_tb is
     
 begin
 
-    uut: entity work.vga_controller(imp) port map(sel => '0', 
-                                                  rst => rst,
-                                                  clk => clk,
-                                                  hsync => hsync, 
-                                                  vsync => vsync, 
-                                                  h_address => h_address, 
-                                                  v_address => v_address,
-                                                  addressing => addressing,
-                                                  rgb => rgb);
+    uut: entity work.vga_controller(imp)
+             generic map(
+                     H_ADDRESSABLE => H_ADDRESSABLE,
+                     H_FRONT_PORCH =>H_FRONT_PORCH,
+                     H_SYNC => H_SYNC,
+                     H_BACK_PORCH => H_BACK_PORCH,
 
+                     V_ADDRESSABLE => V_ADDRESSABLE,
+                     V_FRONT_PORCH => V_FRONT_PORCH,
+                     V_SYNC => V_SYNC,
+                     V_BACK_PORCH => V_BACK_PORCH)
+
+               port map(sel => '0', 
+                      rst => rst,
+                      clk => clk,
+                      hsync => hsync, 
+                      vsync => vsync, 
+                      h_address => h_address, 
+                      v_address => v_address,
+                      addressing => addressing,
+                      rgb => rgb);
+                
     int_h_address <= to_integer(unsigned(h_address));
     int_v_address <= to_integer(unsigned(v_address));
 	 
@@ -83,7 +104,7 @@ begin
 	begin
         wait until rst = '0';  
     
-        wait until int_h_address = 799;
+        wait until int_h_address = H_ADDRESSABLE - 1;
         report "End first row."
         severity note;
 
@@ -129,7 +150,7 @@ begin
             
             wait for (H_ADDRESSABLE - 1)*CLOCK_PERIOD;
             
-			assert int_h_address = 799
+			assert int_h_address = H_ADDRESSABLE - 1
 			report "Wrong h_address at horizontal address end."
 			severity failure;
             
@@ -145,15 +166,22 @@ begin
 	begin
         wait until rst = '0';      
 		
-        wait until int_v_address = 599;
+        wait until int_v_address = V_ADDRESSABLE - 1;
         report "End frame."
         severity note;
-
-        wait for CLOCK_PERIOD*H_PERIOD;
 
         -- Garantees ModelSim evaluates signal on clk edge
         -- and not before
         wait for CLOCK_PERIOD/2;
+
+
+
+        assert int_v_address = V_ADDRESSABLE - 1       
+        report "Simulator ERROR."
+        severity failure;
+
+
+        wait for CLOCK_PERIOD*H_PERIOD;
 
         report "Start TEST_VERTICAL_SYNC_AND_ADDRESS."
         severity note;
@@ -164,7 +192,8 @@ begin
             severity note;
 
         
-            assert int_v_address = 0;
+            wait for 1 ns;
+            assert v_address = "0000000000"
             report "v_address incorrect end."
             severity failure;
         
@@ -172,32 +201,23 @@ begin
 			report "vsync low on start of vertical period."
 			severity failure;
 			
-			wait for V_FRONT_PORCH*CLOCK_PERIOD;
+			wait for V_FRONT_PORCH*H_PERIOD*CLOCK_PERIOD;
 			
 			assert vsync = '0'
-			report "Hsync high on sync pulse."
+			report "Vsync high on sync pulse."
 			severity failure;
 			
-			wait for V_SYNC*CLOCK_PERIOD;
-            
+			wait for V_SYNC*H_PERIOD*CLOCK_PERIOD;            
 			assert vsync = '1'
 			report "Sync pulse larger than expected."
 			severity failure;
             
-            wait for V_BACK_PORCH*CLOCK_PERIOD;
-            
+            wait for V_BACK_PORCH*H_PERIOD*CLOCK_PERIOD;            
 			assert int_v_address = 0
 			report "Wrong v_address at vertical address start."
 			severity failure;
             
-            wait for (V_ADDRESSABLE - 1)*CLOCK_PERIOD;
-            
-			assert int_v_address = 599
-			report "Wrong v_address at vertical address end."
-			severity failure;
-            
-            wait for CLOCK_PERIOD;
-            
+            wait for V_ADDRESSABLE*H_PERIOD*CLOCK_PERIOD;            
             report "End TEST_VERTICAL_SYNC_AND_ADDRESS " & natural'image(n);
             
         end loop;
