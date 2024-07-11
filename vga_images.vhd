709b87df1f774c5871001c61c6f389376c0554d3
Gabriel S.Zanella <gabes.zanella@gmail.com>
2024-07-11 15:01:12 -0300

    Add synchronous behavioural address converter

--- vga_images.vhd
+++ vga_images.vhd
@@ -26,12 +26,14 @@ end entity vga_images;
 architecture imp of vga_images is
     signal int_v_address: integer range 0 to 600;
     signal int_h_address: integer range 0 to 800;
-    signal int_rom_v_address: integer range 0 to 37;
-    signal int_rom_h_address: integer range 0 to 50;
+
     signal rom_v_address: std_logic_vector(V_BUS-1 downto 0);
     signal rom_h_address: std_logic_vector(H_BUS-1 downto 0);
+    
     signal rgb_rom: std_logic_vector(RGB_LENGTH - 1 downto 0);
-
+    
+    signal int_rom_v_address_next: integer range 0 to 37;
+    signal int_rom_h_address_next: integer range 0 to 50;
 begin
     VGA: entity work.vga_controller(imp)
         port map (
@@ -56,90 +58,41 @@ begin
 
     int_h_address <= to_integer(unsigned(h_address));
     int_v_address <= to_integer(unsigned(v_address));
+       
+    process(clk, rst)
+    begin
+        if rst = '1' then
+            rom_v_address <= (others => '0');
+            rom_h_address <= (others => '0');
+        
+        elsif rising_edge(clk) then
+            rom_v_address <= std_logic_vector(to_unsigned(int_rom_v_address_next, V_BUS));
+            rom_h_address <= std_logic_vector(to_unsigned(int_rom_h_address_next, H_BUS));
+        end if;
+    end process;
     
-    rom_v_address <= std_logic_vector(to_unsigned(int_rom_v_address, V_BUS));
-    rom_h_address <= std_logic_vector(to_unsigned(int_rom_h_address, H_BUS));
+
+
+    process(int_h_address)
+    begin
+        for n in 1 to 49 loop
+            if (int_h_address = 799) or (int_h_address = 0) then
+                int_rom_h_address_next <= 0;
+            elsif (n*16) - 1 = int_h_address then
+                int_rom_h_address_next <= n;
+            end if;
+        end loop;
+    end process;
     
-    with int_h_address select
-        int_rom_h_address <=
-            1 when 16,
-            2 when 32,
-            3 when 48,
-            4 when 64,
-            5 when 80,
-            6 when 96,
-            7 when 112,
-            8 when 128,
-            9 when 144,
-            10 when 160,
-            11 when 176,
-            12 when 192,
-            13 when 208,
-            14 when 224,
-            15 when 240,
-            16 when 256,
-            17 when 272,
-            18 when 288,
-            19 when 304,
-            20 when 320,
-            21 when 336,
-            22 when 352,
-            23 when 368,
-            24 when 384,
-            25 when 400,
-            26 when 416,
-            27 when 432,
-            28 when 448,
-            29 when 464,
-            30 when 480,
-            31 when 496,
-            32 when 512,
-            33 when 528,
-            34 when 544,
-            35 when 560,
-            36 when 576,
-            37 when 592,
-            0 when others;
-    
-    with int_v_address select
-        int_rom_v_address <=
-            1 when 16,
-            2 when 32,
-            3 when 48,
-            4 when 64,
-            5 when 80,
-            6 when 96,
-            7 when 112,
-            8 when 128,
-            9 when 144,
-            10 when 160,
-            11 when 176,
-            12 when 192,
-            13 when 208,
-            14 when 224,
-            15 when 240,
-            16 when 256,
-            17 when 272,
-            18 when 288,
-            19 when 304,
-            20 when 320,
-            21 when 336,
-            22 when 352,
-            23 when 368,
-            24 when 384,
-            25 when 400,
-            26 when 416,
-            27 when 432,
-            28 when 448,
-            29 when 464,
-            30 when 480,
-            31 when 496,
-            32 when 512,
-            33 when 528,
-            34 when 544,
-            35 when 560,
-            36 when 576,
-            37 when 592,
-            0 when others;
-            
-end architecture imp;
+    process(int_v_address)
+    begin
+        for n in 1 to 37 loop
+            if (int_v_address = 599) or (int_v_address = 0) then
+                int_rom_v_address_next <= 0;
+            elsif (n*16) - 1 = int_v_address then
+                int_rom_v_address_next <= n;
+            end if;
+        end loop;
+    end process;
+           
+end architecture;
