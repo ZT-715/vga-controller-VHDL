81337f89b742bb954281af70aeef5fc152b7f35e
Gabriel S.Zanella <gabes.zanella@gmail.com>
2024-07-24 06:02:01 -0300

    Fully automate complete gen_sync testbench

--- gen_sync_tb.vhd
+++ gen_sync_tb.vhd
@@ -8,35 +8,37 @@ end entity;
 
 architecture tb of gen_sync_tb is
     
-  -- pixel count of each interval
-	 constant HIGH_START_COUNT: natural := 56; 
-	 constant LOW_COUNT: natural := 120;
-	 constant TOTAL_COUNT: natural := 1040;
+    -- Contagem de pixeis para cada intervalo do gerador de pulso
+	constant HIGH_START_COUNT: natural := 5; 
+	constant LOW_COUNT: natural := 5;
+	constant TOTAL_COUNT: natural := 15;
   
-	 constant COUNTER_LENGTH: natural := 11;
+	constant COUNTER_LENGTH: natural := 5;
 
     constant LIMIT: natural := TOTAL_COUNT;
     
-	 signal rst, clk: std_logic := '0';
+	signal rst, clk: std_logic := '0';
     signal sync: std_logic;
     signal c: std_logic_vector(COUNTER_LENGTH - 1 downto 0);
 
 begin
     counter: entity work.gen_counter(imp)
-					generic map(LIMIT, COUNTER_LENGTH)
+					generic map(LIMIT,
+                                COUNTER_LENGTH)
 					port map(rst => rst,
-								clk => clk,
-								en => '1',
-								y => c);
+                             clk => clk,
+                             en => '1',
+                             y => c);
 
     uut: entity work.gen_sync(imp) 
                 generic map(HIGH_START_COUNT => HIGH_START_COUNT,
-									 LOW_COUNT => LOW_COUNT,
-									 TOTAL_COUNT => TOTAL_COUNT) 
+                            LOW_COUNT => LOW_COUNT,
+                            TOTAL_COUNT => TOTAL_COUNT,
+                            COUNTER_LENGTH => COUNTER_LENGTH) 
                 port map(rst => rst,
-								 clk => clk, 
-								 c => c,
-								 sync => sync);
+                         clk => clk, 
+                         c => c,
+                         sync => sync);
 
     clk <= not clk after 10 ns;
 
@@ -51,49 +53,33 @@ begin
 	  wait;
 	 end process;
 
-    process
+    TEST: process
     begin
         wait until rst = '0';
-        assert sync = '1'
-        report "Synchrony signal started LOW."
-        severity failure;
+        
+        for i in natural range 0 to 2 loop
+            assert sync = '1'
+            report time'image(now) & " - Signal low after reset."
+            severity failure;
 
-        wait for HIGH_START_COUNT* 20 ns;
-		  
-        assert sync = '0'
-        report "Synchrony signal did't changed from high to low."
-        severity failure;
-		  
-		  wait for LOW_COUNT * 20 ns;
-		  
-        assert sync = '1'
-        report "Synchrony signal did't changed to high."
-        severity failure;
-		  
-		  wait;
+            wait for HIGH_START_COUNT * 20 ns;
+              
+            assert sync = '0'
+            report time'image(now) & " - Sync pulse failed to start."
+            severity failure;
+              
+            wait for LOW_COUNT * 20 ns;
+              
+            assert sync = '1'
+            report time'image(now) & " - Sync pulse failed to end."
+            severity failure;
+            
+            wait for (TOTAL_COUNT - LOW_COUNT - HIGH_START_COUNT)* 20 ns;
+            report time'image(now) & " - End of cycle " & integer'image(i) & ".";
+		 end loop;
+         
+         report time'image(now) & " - Success!";
+		 wait;
     end process;
-
-	process
-	begin
-		wait until rst = '0';
-		
-		wait for TOTAL_COUNT * 20 ns;
-		
-		assert to_integer(unsigned(c)) = 0
-		report "Counter didn't end at LIMIT"
-		severity failure;
-		
-		wait for HIGH_START_COUNT* 20 ns;  
-		
-		assert sync = '0'
-		report "Synchrony signal did't changed from high to low."
-		severity failure;
-		
-		assert now = 0 ns
-		report "Tests ended successfully"
-		severity note;
-		
-		wait;
-	end process;
 	 
 end architecture;
